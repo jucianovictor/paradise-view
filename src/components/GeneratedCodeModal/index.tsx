75a817441f5bdda0c24da5a47e8be8139ea470ab
Juciano Oliveira <jucianovictor95@gmail.com>
2024-03-25 16:34:04 -0300

    feat: add "copied!" state for the copy button

--- src/components/GeneratedCodeModal/index.tsx
+++ src/components/GeneratedCodeModal/index.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import { closeButton, codeContainer, container, copyCodeButton, header } from './index.css';
 
 export interface GeneratedCodeModalProps {
@@ -6,15 +9,17 @@ export interface GeneratedCodeModalProps {
 }
 
 export const GeneratedCodeModal = ({ code, onClickClose }: GeneratedCodeModalProps) => {
+	const [isCopied, setIsCopied] = useState(false);
 	const copyTextToClipboard = () => {
 		navigator.clipboard.writeText(code);
+		setIsCopied(true);
 	};
 
 	return (
 		<div className={container} aria-modal aria-hidden tabIndex={-1} role="dialog">
 			<div className={header}>
 				<button type="button" className={copyCodeButton} onClick={copyTextToClipboard}>
-					Copy
+					{isCopied ? 'Copied!' : 'Copy'}
 				</button>
 				<button type="button" className={closeButton} onClick={onClickClose}>
 					<span aria-hidden="true">&times;</span>
